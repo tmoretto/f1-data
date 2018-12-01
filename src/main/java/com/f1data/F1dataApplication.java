2e6501922d78db096322fb84a88de8bade94d562
Tiago Moretto <moretto.tiago@gmail.com>
2018-12-01 10:31:15 -0200

    initializing project

---
+++ src/main/java/com/f1data/F1dataApplication.java
@@ -0,0 +1,12 @@
+package com.f1data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1dataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1dataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/f1data/test/F1dataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.f1data.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class F1dataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
